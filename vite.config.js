30d9422953cb77e1867a9bda8299ee82ec857e94
moelhouary <Mohammed.ELHOUARY@etu.uca.fr>
2026-02-10 00:03:00 +0100

    Configure GitLab Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Mohammed-ELHOUARY/",
+  base: "/Mohammed-ELHOUARY-Portfolio/",
   plugins: [react()],
 });
